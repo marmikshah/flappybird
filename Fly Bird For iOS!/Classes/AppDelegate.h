6b6033d8dfd80863bc5104911f65d01c3707cca1
Marmik Shah <marmikshah@icloud.com>
2014-03-24 19:11:31 +0530

    Initial Commit

---
+++ Fly Bird For iOS!/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Fly Bird For iOS!
+//
+//  Created by Marmik Shah on 24/03/14.
+//  Copyright Marmik 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Fly Bird For iOS!/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Fly Bird For iOS!
+//
+//  Created by Marmik Shah on 24/03/14.
+//  Copyright Marmik 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
